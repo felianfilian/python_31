a6a5ad6236d97e529c00be9470384aa32b5bd23b
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-24 00:16:41 +0100

    text setup

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
-FONT_01 = ("Arial", 24, "bold")
+FONT_01 = ("Arial", 40, "italic")
+FONT_02 = ("Arial", 60, "bold")
 
 window = Tk()
 window.title("Flashcards")
@@ -11,10 +12,9 @@ canvas = Canvas(width=800, height=526)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Title", font=FONT_01)
+canvas.create_text(400, 250, text="word", font=FONT_02)
 canvas.grid(row=0, column=0)
-
-# label_01 = Label(text="Flashcards", font=FONT_01)
-# label_01.grid(row=0, column=1)
 
 window.mainloop()
 
