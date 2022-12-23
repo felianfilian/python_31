64ac9471c7559dde6fc1089926c31b4c4efbef52
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-23 23:57:08 +0100

    window layout

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ FONT_01 = ("Arial", 24, "bold")
 
 window = Tk()
 window.title("Flashcards")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 window.minsize(width=800, height=600)
 
 label_01 = Label(text="Flashcards", font=FONT_01)
