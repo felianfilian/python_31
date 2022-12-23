220acf12731cfe3d4956cbac1b368ffb5d3377f1
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-24 00:02:27 +0100

    first gui layout

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ FONT_01 = ("Arial", 24, "bold")
 window = Tk()
 window.title("Flashcards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-window.minsize(width=800, height=600)
 
-label_01 = Label(text="Flashcards", font=FONT_01)
-label_01.grid(row=0, column=1)
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.grid(row=0, column=0)
+
+# label_01 = Label(text="Flashcards", font=FONT_01)
+# label_01.grid(row=0, column=1)
 
 window.mainloop()
 
