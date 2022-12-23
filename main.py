b051c97c0bc841c2a7cbb06a0b893c00b7d14baf
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-23 23:51:35 +0100

    tkinter setup

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flashcards")
+window.minsize(width=800, height=600)
+
+canvas = Canvas()
+
+window.mainloop()
+
 
