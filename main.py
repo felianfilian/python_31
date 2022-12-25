af4e155fce7622ae71a77b2f5d4a82e0d10e9fc2
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-25 10:31:18 +0100

    canvas text update

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ FONT_01 = ("Arial", 40, "italic")
 FONT_02 = ("Arial", 60, "bold")
 
 data = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
-chosen_word = random.choice(data)
-print(chosen_word)
 
 def next_card():
-    print(chosen_word["French"])
-    print(chosen_word["English"])
-
-next_card()
+    chosen_word = random.choice(data)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=chosen_word["French"])
 
 window = Tk()
 window.title("Flashcards")
@@ -24,8 +21,8 @@ canvas = Canvas(width=800, height=526)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=FONT_01)
-canvas.create_text(400, 250, text="word", font=FONT_02)
+card_title = canvas.create_text(400, 150, text="Title", font=FONT_01)
+card_word = canvas.create_text(400, 250, text="word", font=FONT_02)
 canvas.grid(row=0, column=0, columnspan=2)
 
 right_image = PhotoImage(file="images/right.png")
@@ -35,6 +32,8 @@ check_button.grid(row=1, column=0)
 cross_image = PhotoImage(file="images/wrong.png")
 cancel_button = Button(image=cross_image, command=next_card)
 cancel_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
 
